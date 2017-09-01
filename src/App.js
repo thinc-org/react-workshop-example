54fde60d8fad50074ad7a4b6e9ab6442bd732370
naisk <naisk133@gmail.com>
2017-09-01 15:20:25 +0700

    step 3: create a counter component to demonstrate how react state works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      value: 0
+    }
+    this.incrementCounter = this.incrementCounter.bind(this)
+  }
+  incrementCounter() {
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
   render() {
     return (
       <div>
-        <input type="text"></input>
-        <button>Add Subject</button>
+      <div>
+        <h3>{this.state.value}</h3>
+        <button onClick={this.incrementCounter}>+</button>
+      </div>
+      <br/>
+      <div>
+        <input type="text"></input>   
+        <button onClick={ () => {alert('Add Subject Clicked!')}}>Add Subject</button>
+      </div>
       </div>
     )
   }
